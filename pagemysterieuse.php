56769a3f6fc412291c2b1ecf6f57aef999a33caa
jonathan <deroo.jon@gmail.com>
2026-02-07 12:44:45 +0100

    Rajouter un div autour le carousel pour fixer l'hauteur et rajouter du commentaire en dessous

--- pagemysterieuse.php
+++ pagemysterieuse.php
@@ -11,23 +11,23 @@ require "header.php";
 <!--carousel-->
 <br><br>
 
-<h1>La Page Mysterieuse sur les mystéres inconnu qui ne sonts pas connu</h1>
+<h1 style="text-align: center;">La Page Mysterieuse sur les mystéres inconnu qui ne sonts pas connu</h1>
 
-
+<div class = carousel-container>
 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner" >
 
-        <div class="carousel-item active" data-bs-interval="1000"><div class="carrousel-image">
-                <img src="/assets/images/nessie.webp" class="d-block w-100" alt="..." style="border-radius: 20px;"> </div>
+        <div class="carousel-item active" data-bs-interval="3000"><div class="carrousel-image">
+                <img src="/assets/images/nessie.webp" class="d-block w-100" alt="..." style="border-radius: 20px;"> <h1 style="text-align: center; padding-top: 1%"> Un tronc d'arbre!</h1></div>
         </div>
-        <div class="carousel-item " data-bs-interval="1000"><div class="carrousel-image">
-                <img src="/assets/images/flatearth.webp" class="d-block w-100" alt="..." style="border-radius: 20px;"></div>
+        <div class="carousel-item " data-bs-interval="3000"><div class="carrousel-image">
+                <img src="/assets/images/flatearth.webp" class="d-block w-100" alt="..." style="border-radius: 20px;"><h1 style="text-align: center; padding-top: 1%"> Des gens qui sonts plates!</h1></div>
         </div>
-        <div class="carousel-item " data-bs-interval="1000"><div class="carrousel-image">
-                <img src="/assets/images/covidconspiracy.jpg" class="d-block w-100" alt="..." style="border-radius: 20px;"></div>
+        <div class="carousel-item " data-bs-interval="3000"><div class="carrousel-image">
+                <img src="/assets/images/covidconspiracy.jpg" class="d-block w-100" alt="..." style="border-radius: 20px;"><h1 style="text-align: center; padding-top: 1%"> Est-ce que les forêts Flamands contient-ils des virus encore inconnus??</h1></div>
         </div>
-        <div class="carousel-item " data-bs-interval="1000"><div class="carrousel-image">
-                <img src="/assets/images/brigittemacron.jpg" class="d-block w-100" alt="..." style="border-radius: 20px;"></div>
+        <div class="carousel-item " data-bs-interval="3000"><div class="carrousel-image">
+                <img src="/assets/images/brigittemacron.jpg" class="d-block w-100" alt="..." style="border-radius: 20px;"><h1 style="text-align: center; padding-top: 1%"> Miss World 2026??</h1></div>
         </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
@@ -39,6 +39,9 @@ require "header.php";
         <span class="visually-hidden">Next</span>
     </button>
 </div>
+
+
+</div class="carousel-container">
 
 
 
